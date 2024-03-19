9ff402c62236e158879ed5efd8740effab4fb642
toshibaaaaa <kickback0713@gmail.com>
2024-03-19 16:45:02 +0800

    fixed: hover sa sidebar, at meron onti sa room modal

--- my-project/src/components/RoomModal.jsx
+++ my-project/src/components/RoomModal.jsx
@@ -1,177 +1,251 @@
-import React, { useState, useEffect } from 'react'; // Import useState
-import { Multiselect } from 'multiselect-react-dropdown';
-
-const RoomModal = ({ closeModal, addNewRoom, roomToEdit, roomId, updateRoom, deleteRoom }) => {
-
+import React, { useState, useEffect } from "react"; // Import useState
+import { MultiSelect } from "primereact/multiselect";
+import axios from "axios";
+const RoomModal = ({ closeModal }) => {
+  const [room, setRoom] = useState({
+    room_name: "",
+    price: 0,
+    miniDes: "",
+    description: "",
+    roomAmenities: [],
+    buildingAmenities: [],
+    image_name: [],
+  });
+  const [files, setFile] = useState([]);
   const roomAmenities = [
-    { Amenities: 'Air-Condition', id: 1 },
-    { Amenities: 'Unlimited Wifi', id: 2 },
-    { Amenities: 'Smart TV/4k UHD', id: 3 },
-    { Amenities: 'Board games', id: 4 },
-    { Amenities: 'Double size bed with Pull out bed', id: 5 },
-    { Amenities: 'Extra blanket', id: 6 },
-    { Amenities: 'Bathroom towel', id: 7 },
-    { Amenities: 'Shower Heater', id: 8 },
-    { Amenities: 'Complete Kitchenware and Utensils', id: 9 },
-    { Amenities: 'Refrigerator', id: 10 },
-    { Amenities: 'Microwave', id: 11 },
-    { Amenities: 'Rice Cooker', id: 12 },
-    { Amenities: 'Induction Cooker', id: 13 },
-    { Amenities: 'Electric Kettle', id: 14 },
-    { Amenities: 'Free Coffee', id: 15 },
-    { Amenities: 'Netflix and Karaoke', id: 16 },
-    { Amenities: 'Game Cards', id: 17 }
-  ]
-  const [roomOptions] = useState(roomAmenities);
+    { Amenities: "Air-Condition" },
+    { Amenities: "Unlimited Wifi" },
+    { Amenities: "Smart TV/4k UHD" },
+    { Amenities: "Board games" },
+    { Amenities: "Double size bed with Pull out bed" },
+    { Amenities: "Extra blanket" },
+    { Amenities: "Bathroom towel" },
+    { Amenities: "Shower Heater" },
+    { Amenities: "Complete Kitchenware and Utensils" },
+    { Amenities: "Refrigerator" },
+    { Amenities: "Microwave" },
+    { Amenities: "Rice Cooker" },
+    { Amenities: "Induction Cooker" },
+    { Amenities: "Electric Kettle" },
+    { Amenities: "Free Coffee" },
+    { Amenities: "Netflix and Karaoke" },
+    { Amenities: "Game Cards" },
+  ];
+  // const [roomOptions] = useState(roomAmenities);
 
-  const buildingAmenities = [,
-    { Amenities: '🏊🏻‍♂️ Swimming Pool (adult & kids)', id: 1 },
-    { Amenities: '🏀 Basketball Court', id: 2 },
-    { Amenities: '🏋🏻 Gym ', id: 3 },
-    { Amenities: '🏙️ Roof Deck', id: 4 },
-    { Amenities: '🛝 Playground', id: 5 },
-    { Amenities: '🛖 Gazebo', id: 6 },
-    { Amenities: '🚬 Smoking Area', id: 7 },
-    { Amenities: '🔥 Grill Pit', id: 8 }
-  ]
-  const [buildingOptions] = useState(buildingAmenities);
+  const buildingAmenities = [
+    ,
+    { Amenities: "🏊🏻‍♂️ Swimming Pool (adult & kids)" },
+    { Amenities: "🏀 Basketball Court" },
+    { Amenities: "🏋🏻 Gym " },
+    { Amenities: "🏙️ Roof Deck" },
+    { Amenities: "🛝 Playground" },
+    { Amenities: "🛖 Gazebo" },
+    { Amenities: "🚬 Smoking Area" },
+    { Amenities: "🔥 Grill Pit" },
+  ];
+  // const [buildingOptions] = useState(buildingAmenities);
 
-  const onSelectFile = (event) => {
-    const selectedFiles = event.target.files;
-    const selectedFilesArray = Array.from(selectedFiles);
-    const imagesArray = selectedFilesArray.map((file) => {
-      return URL.createObjectURL(file);
-    });
-    setSelectedImages((prevImages) => [...prevImages, ...imagesArray]); // Append new images to existing ones
+  // const onSelectFile = (event) => {
+  //   const selectedFiles = event.target.files;
+  //   const selectedFilesArray = Array.from(selectedFiles);
+  //   const imagesArray = selectedFilesArray.map((file) => {
+  //     return URL.createObjectURL(file);
+  //   });
+  //   setSelectedImages((prevImages) => [...prevImages, ...imagesArray]); // Append new images to existing ones
+  // };
+  // const [selectedImages, setSelectedImages] = useState([]);
+
+  // const [selectedRoomAmenities, setSelectedRoomAmenities] = useState([]);
+  // const [selectedBuildingAmenities, setSelectedBuildingAmenities] = useState(
+  //   []
+  // );
+
+  // useEffect(() => {
+  //   if (roomToEdit) {
+  //     setSelectedImages([roomToEdit.src]);
+  //     setSelectedRoomAmenities(roomToEdit.roomAmenities || []);
+  //     setSelectedBuildingAmenities(roomToEdit.buildingAmenities || []);
+  //   } else {
+  //     setSelectedImages([]);
+  //     setSelectedRoomAmenities([]);
+  //     setSelectedBuildingAmenities([]);
+  //   }
+  // }, [roomToEdit]);
+
+  //
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setRoom({ ...room, [e.target.name]: e.target.value });
   };
-  const [selectedImages, setSelectedImages] = useState([]);
 
-  const [selectedRoomAmenities, setSelectedRoomAmenities] = useState([]);
-  const [selectedBuildingAmenities, setSelectedBuildingAmenities] = useState([]);
+  const handleFileInputChange = (e) => {
+    const selectedFiles = Array.from(e.target.files);
+    setRoom([...room, ...selectedFiles]);
+  };
 
-  useEffect(() => {
-    if (roomToEdit) {
-      setSelectedImages([roomToEdit.src]);
-      setSelectedRoomAmenities(roomToEdit.roomAmenities || []);
-      setSelectedBuildingAmenities(roomToEdit.buildingAmenities || []);
-    } else {
-      setSelectedImages([]); 
-      setSelectedRoomAmenities([]);
-      setSelectedBuildingAmenities([]);
-    }
-  }, [roomToEdit]);
+  const handleDeleteImage = (index) => {
+    setFile([...files.slice(0, index), ...files.slice(index + 1)]);
+  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    const newRoomData = {
-      title: formData.get('title'),
-      price: formData.get('price'),
-      address: formData.get('address'),
-      miniDes: formData.get('miniDes'),
-      description: formData.get('description'),
-      roomAmenities: selectedRoomAmenities,
-      buildingAmenities: selectedBuildingAmenities,
-      src: selectedImages
-    };
-
-    if (roomId) {
-      updateRoom(roomId, newRoomData);
-    } else {
-      addNewRoom(newRoomData);
-    }
-
+  const handleModal = () => {
     closeModal();
   };
-
-  const handleDelete = () => {
-    deleteRoom(roomId);
-    closeModal();
-  };
-
 
   return (
-    <div id='container' className='fixed top-0 left-0  w-full h-full bg-slate-300 bg-opacity-40 items-center justify-center flex' onClick={(e) => { if (e.target.id === "container") closeModal(); }}>
-      <div id='modal' className='rounded-md p-3 bg-actNav w-5/6 h-5/6 overflow-auto scrollbar-thin scrollbar-webkit'>
-        <h1 className='text-2xl font-bold mt-2'>Edit Room</h1>
-        <form onSubmit={handleSubmit}>
+    <div
+      id="container"
+      className="fixed top-0 left-0  w-full h-full bg-slate-300 bg-opacity-40 items-center justify-center flex"
+      onClick={(e) => {
+        if (e.target.id === "container") closeModal();
+      }}
+    >
+      <div
+        id="modal"
+        className="rounded-md p-3 bg-actNav w-5/6 h-5/6 overflow-auto scrollbar-thin scrollbar-webkit"
+      >
+        <h1 className="text-2xl font-bold mt-2">Edit Room</h1>
+        <form>
           <label className="flex flex-col justify-center items-center">
-            <div className='flex flex-col justify-center items-center bg-white text-darkText rounded-xl border border-solid p-3 aspect-square w-40 cursor-pointer'>
+            <div className="flex flex-col justify-center items-center bg-white text-darkText rounded-xl border border-solid p-3 aspect-square w-40 cursor-pointer">
               + Add Image <br />
-              <span className='font-lighter text-sm'>Up to 10 Images</span>
-              <input type='file' id='images' className='hidden' onChange={onSelectFile} multiple accept='image/jpg, image/jpeg, image/webp' />
+              <span className="font-lighter text-sm">Up to 10 Images</span>
+              <input
+                type="file"
+                id="images"
+                name="image_name"
+                className="hidden"
+                value={room.image_name}
+                onChange={handleFileInputChange}
+                multiple
+                accept="image/jpg, image/jpeg, image/webp"
+              />
             </div>
           </label>
-          <div className='flex flex-wrap justify-center items-center mt-3'>
-            {selectedImages &&
-              selectedImages.map((image, index) => {
-                return (
-                  <div key={image} className=''>
-                    <img src={image} className='h-40 m-2' alt='upload' defaultValue={roomToEdit ? roomToEdit.image : ''} />
-                    <button
-                      className={'bg-notActText hover:bg-yellow-700 font-sm uppercase rounded-lg'}
-                      onClick={() =>
-                        setSelectedImages(selectedImages.filter((e) => e !== image))}
-                    >
-                      Delete Image
-                    </button>
-                  </div>
-                )
-              })}
+          <div className="flex flex-wrap justify-center items-center mt-3">
+            {files.map((image, index) => (
+              <div key={index} className="m-2">
+                <img
+                  src={URL.createObjectURL(image)}
+                  className="h-40"
+                  alt={`Uploaded Image ${index}`}
+                />
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-2"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleDeleteImage(index);
+                  }}
+                >
+                  Delete Image
+                </button>
+              </div>
+            ))}
           </div>
           <div className="relative p-6 flex-auto">
             <div className="flex flex-wrap">
               <div className="w-1/2 pr-2">
-                <label className="block text-black text-sm font-semibold mt-1"> Title: </label>
-                <input name="title" className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText" defaultValue={roomToEdit ? roomToEdit.title : ''} />
+                <label className="block text-black text-sm font-semibold mt-1">
+                  {" "}
+                  Title:{" "}
+                </label>
+                <input
+                  name="room_name"
+                  className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText"
+                  // defaultValue={roomToEdit ? roomToEdit.title : ""}
+                  onChange={handleChange}
+                />
               </div>
               <div className="w-1/2 pl-2">
-                <label className="block text-black text-sm font-semibold mt-1"> Price: </label>
-                <input name="price" className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText" defaultValue={roomToEdit ? roomToEdit.price : ''} />
+                <label className="block text-black text-sm font-semibold mt-1">
+                  {" "}
+                  Price:{" "}
+                </label>
+                <input
+                  name="price"
+                  className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText"
+                  // defaultValue={roomToEdit ? roomToEdit.price : ""}
+                  onChange={handleChange}
+                />
               </div>
             </div>
             <div className="flex flex-wrap">
               <div className="w-1/2 pr-2">
-                <label className="block text-black text-sm font-semibold mt-1"> Address: </label>
-                <input name="address" className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText" defaultValue={roomToEdit ? roomToEdit.address : ''} />
+                <label className="block text-black text-sm font-semibold mt-1">
+                  {" "}
+                  Address:{" "}
+                </label>
+                <input
+                  name="address"
+                  className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText"
+                  // defaultValue={roomToEdit ? roomToEdit.address : ""}
+                  onChange={handleChange}
+                />
               </div>
               <div className="w-1/2 pl-2">
-                <label className="block text-black text-sm font-semibold mt-1"> Mini Description: </label>
-                <input name="miniDes" className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText" defaultValue={roomToEdit ? roomToEdit.miniDes : ''} />
+                <label className="block text-black text-sm font-semibold mt-1">
+                  {" "}
+                  Mini Description:{" "}
+                </label>
+                <input
+                  name="miniDes"
+                  className="shadow appearance-none border rounded w-full py-1 px-1 bg-white text-darkText"
+                  // defaultValue={roomToEdit ? roomToEdit.miniDes : ""}
+                  onChange={handleChange}
+                />
               </div>
-              <label className="block text-black text-sm font-semibold mt-1"> Description: </label>
-              <textarea id='description'
+              <label className="block text-black text-sm font-semibold mt-1">
+                {" "}
+                Description:{" "}
+              </label>
+              <textarea
+                id="description"
                 type="text"
-                className='mt-2 bg-white text-darkText text-mg flex capitalize-first p-2 items-center justify-between shadow rounded-lg resize-none scrollbar-thin scrollbar-webkit'
-                style={{ width: "100%", height: "100px", wordWrap: "break-word" }} defaultValue={roomToEdit ? roomToEdit.description : ''} />
+                className="mt-2 bg-white text-darkText text-mg flex capitalize-first p-2 items-center justify-between shadow rounded-lg resize-none scrollbar-thin scrollbar-webkit"
+                style={{
+                  width: "100%",
+                  height: "100px",
+                  wordWrap: "break-word",
+                }}
+                onChange={handleChange}
+                name="description"
+                // defaultValue={roomToEdit ? roomToEdit.description : ""}
+              />
             </div>
 
             <div className="flex flex-wrap">
               <div className="w-1/2 pr-2">
-                <label className="block text-black text-sm font-semibold mt-1"> Room Amenities: </label>
-                <Multiselect
-                id='roomAmenities'
-                options={roomAmenities}
-                selectedValues={selectedRoomAmenities} 
-                onSelect={(selectedList) => setSelectedRoomAmenities(selectedList)}
-                onRemove={(selectedList) => setSelectedRoomAmenities(selectedList)}
-                displayValue='Amenities'
-                className='shadow appearance-none border rounded w-full bg-white text-darkText'
-              />
+                <label className="block text-black text-sm font-semibold mt-1">
+                  {" "}
+                  Room Amenities:{" "}
+                </label>
+                <MultiSelect
+                  name="roomAmenities"
+                  value={room.roomAmenities}
+                  onChange={handleChange}
+                  options={roomAmenities}
+                  optionLabel="Amenities"
+                  filter
+                  placeholder="Select Amenities"
+                  className="shadow appearance-none border rounded w-full bg-white text-darkText"
+                />
               </div>
               <div className="w-1/2 pl-2">
-                <label className="block text-black text-sm font-semibold mt-1"> Building Amenities: </label>
-                <Multiselect
-                id='buildingAmenities'
-                options={buildingAmenities}
-                selectedValues={selectedBuildingAmenities} 
-                onSelect={(selectedList) => setSelectedBuildingAmenities(selectedList)}
-                onRemove={(selectedList) => setSelectedBuildingAmenities(selectedList)}
-                displayValue='Amenities'
-                className='shadow appearance-none border rounded w-full bg-white text-darkText'
-              />
+                <label className="block text-black text-sm font-semibold mt-1">
+                  {" "}
+                  Building Amenities:{" "}
+                </label>
+                <MultiSelect
+                  value={room.buildingAmenities}
+                  onChange={handleChange}
+                  name="buildingAmenities"
+                  options={buildingAmenities}
+                  optionLabel="Amenities"
+                  filter
+                  placeholder="Select Amenities"
+                  className="shadow appearance-none border rounded w-full bg-white text-darkText"
+                />
               </div>
             </div>
           </div>
@@ -179,26 +253,35 @@ const RoomModal = ({ closeModal, addNewRoom, roomToEdit, roomId, updateRoom, del
             <button
               className="text-darkText background-transparent font-bold uppercase px-4 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
               type="button"
-              onClick={closeModal} > Close
+              onClick={handleModal}
+            >
+              {" "}
+              Close
             </button>
 
-            {roomId && ( // Render delete button only if roomId exists
+            {/* {roomId && ( // Render delete button only if roomId exists
               <button
                 className="text-white bg-red-500 active:bg-red-600 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
                 type="button"
-                onClick={handleDelete}> Delete
+                onClick={handleDelete}
+              >
+                {" "}
+                Delete
               </button>
-            )}
+            )} */}
             <button
               className="text-white bg-notActText active:bg-yellow-700 font-bold uppercase text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-              type="submit" > Submit
+              type="submit"
+            >
+              {" "}
+              Submit
             </button>
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 //oks na ADD EDIT DELETE
-export default RoomModal
+export default RoomModal;
--- my-project/src/pages/Rooms.jsx
+++ my-project/src/pages/Rooms.jsx
@@ -1,124 +1,102 @@
-import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar'
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
 import { FaPlus } from "react-icons/fa";
-import br1 from '../assets/br1.jpg' 
-import br2 from '../assets/br2.jpg' 
-import RoomModal from '../components/RoomModal';
+import br1 from "../assets/br1.jpg";
+import br2 from "../assets/br2.jpg";
+import RoomModal from "../components/RoomModal";
 
 const Rooms = () => {
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [roomToEdit, setRoomToEdit] = useState(null);
 
-  const toggleModal = (room = null) => {
-    setRoomToEdit(room); // Set the room to edit
-    setIsModalOpen(!isModalOpen);
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
-
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
   const updateRoom = (roomId, updatedRoomData) => {
     // Update the room data in the state
-    setData(data.map(room => 
-      room.id === roomId ? { ...room, ...updatedRoomData } : room
-    ));
+    setData(
+      data.map((room) =>
+        room.id === roomId ? { ...room, ...updatedRoomData } : room
+      )
+    );
   };
 
-  const [data, setData] = useState([
-    {
-      id: 1,
-      src: br1,
-      title: '1 Bedroom Unit with Balcony facing Amenities',
-      price: '₱ Price per night',
-      address: 'asdas',
-      miniDes: 'asd',
-      description: 'saddcvsd',
-      roomAmenities: ['Board games', 'Air-Condition'],
-      buildingAmenities: ''
-    },
-    {
-      id: 2,
-      src: br2,
-      title: '2 Bedroom Unit with Balcony',
-      price: '₱ Price per night',
-      address: '',
-      miniDes: '',
-      description: '',
-      roomAmenities: '',
-      buildingAmenities: ''
-    },
-    {
-      id: 3,
-      src: br1,
-      title: '1 Bedroom Unit with Balcony facing Amenities',
-      price: '₱ Price per night',
-      address: '',
-      miniDes: '',
-      description: '',
-      roomAmenities: '',
-      buildingAmenities: ''
-    },
-    {
-      id: 4,
-      src: br2,
-      title: '2 Bedroom Unit with Balcony',
-      price: '₱ Price per night',
-      address: '',
-      miniDes: '',
-      description: '',
-      roomAmenities: '',
-      buildingAmenities: ''
-    },
-  ]);
+  const [data, setData] = useState([]);
 
-  const addNewRoom = (newRoom) => {
-    setData([...data, { ...newRoom, id: data.length + 1 }]);
-  };
+  // const addNewRoom = (newRoom) => {
+  //   setData([...data, { ...newRoom, id: data.length + 1 }]);
+  // };
 
-  const deleteRoom = (roomId) => {
-    // Filter out the room with the given roomId
-    setData(data.filter(room => room.id !== roomId));
-  };
+  // const deleteRoom = (roomId) => {
+  //   // Filter out the room with the given roomId
+  //   setData(data.filter((room) => room.id !== roomId));
+  // };
 
   return (
     <>
-      <section className='w-4/5 grow bg-backColor h-screen overflow-y-auto flex flex-col justify-start items-center gap-2 p-4 scrollbar-thin scrollbar-webkit'>
-        <div className='flex flex-col bg-mainCol border-b-[1px] border-mainBorder p-4 w-full h-screen rounded-xl justify-between items-center shadow  overflow-y-auto scrollbar-thin scrollbar-webkit'>
-            <div className="w-full flex flex-col sm:flex-row items-start justify-start">
-                <h1 className='text-2xl text-act-text font-semibold'>Manage Rooms </h1>
-            </div>
-            <div className='grid sm:grid-cols-3 grid-cols-1 w-full h-screen mt-3 overflow-y-auto scrollbar-thin scrollbar-webkit'>
-              {data.map(room => (
-                <div id='rooms' className='featured flex justify-center' key={room.id}>
-                  <div id='roomEdit' onClick={() => toggleModal(room)} className='featured-item w-5/6 h-5/6 relative bg-white rounded-xl overflow-hidden Rounded-xl gap-5 
-                  transition-transform transfrom hover:rotate-[-3deg] hover:scale-105  shadow'>
-                    <img src={room.src} className='object-cover w-full h-3/4 rounded-xl' alt={room.title}></img>
-                    <h1 className='m-1 g-2 text-actText font-semibold'>{room.title}</h1>
-                    <h2 className='m-1 g-2 text-darkText font-medium'>{room.price}</h2>
-                  </div>
-                </div>
-              ))}
-              <div className='flex justify-center'>
-                <div id='addNew' onClick={toggleModal} className='w-5/6 h-5/6 relative bg-white rounded-full flex flex-col items-center justify-center hover:bg-darkText hover:opacity-75'>
-                  <FaPlus size={50}/>
-                  <h1 className='mt-2 text-darkText font-semibold'>Add a New Room</h1>
+      <section className="w-4/5 grow bg-backColor h-screen overflow-y-auto flex flex-col justify-start items-center gap-2 p-4 scrollbar-thin scrollbar-webkit">
+        <div className="flex flex-col bg-mainCol border-b-[1px] border-mainBorder p-4 w-full h-screen rounded-xl justify-between items-center shadow  overflow-y-auto scrollbar-thin scrollbar-webkit">
+          <div className="w-full flex flex-col sm:flex-row items-start justify-start">
+            <h1 className="text-2xl text-act-text font-semibold">
+              Manage Rooms{" "}
+            </h1>
+          </div>
+          <div className="grid sm:grid-cols-3 grid-cols-1 w-full h-screen mt-3 overflow-y-auto scrollbar-thin scrollbar-webkit">
+            {data.map((room) => (
+              <div
+                id="rooms"
+                className="featured flex justify-center"
+                key={room.id}
+              >
+                <div
+                  id="roomEdit"
+                  className="featured-item w-5/6 h-5/6 relative bg-white rounded-xl overflow-hidden Rounded-xl gap-5 
+                  transition-transform transfrom hover:rotate-[-3deg] hover:scale-105  shadow"
+                >
+                  <img
+                    src={room.src}
+                    className="object-cover w-full h-3/4 rounded-xl"
+                    alt={room.title}
+                  ></img>
+                  <h1 className="m-1 g-2 text-actText font-semibold">
+                    {room.title}
+                  </h1>
+                  <h2 className="m-1 g-2 text-darkText font-medium">
+                    {room.price}
+                  </h2>
                 </div>
               </div>
-              {isModalOpen && 
-                <RoomModal 
-                closeModal={toggleModal} 
-                addNewRoom={addNewRoom} 
-                roomToEdit={roomToEdit} 
-                roomId={roomToEdit ? roomToEdit.id : null} 
-                updateRoom={updateRoom}  
-                deleteRoom={deleteRoom}
-                setData={setData} // Pass the setData function
-              />
-              }
+            ))}
+            <div className="flex justify-center">
+              <div
+                id="addNew"
+                onClick={openModal}
+                className="w-5/6 h-5/6 relative bg-white rounded-full flex flex-col items-center cursor-pointer justify-center hover:bg-darkText hover:text-white hover:opacity-75 "
+              >
+                <FaPlus size={50} />
+                <h1 className="mt-2 font-semibold ">Add a New Room</h1>
+              </div>
             </div>
+            {isModalOpen && (
+              <RoomModal
+                closeModal={closeModal}
+                // addNewRoom={addNewRoom}
+                // roomToEdit={roomToEdit}
+                // roomId={roomToEdit ? roomToEdit.id : null}
+                // updateRoom={updateRoom}
+                // deleteRoom={deleteRoom}
+                // setData={setData} // Pass the setData function
+              />
+            )}
+          </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
 //oks na ADD EDIT DELETE
-export default Rooms
+export default Rooms;

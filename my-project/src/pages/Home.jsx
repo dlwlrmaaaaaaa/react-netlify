44e1eb1e10a41c83fda37d645247cfb0d0acfcd1
Justin <bebe@gmail.com>
2024-05-04 11:53:32 +0800

    commit

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -11,7 +11,7 @@ import rooms from "../JSON/Room.json";
 import { useStateContext } from "../contexts/contextProvider";
 import axiosClient from "../axios";
 import getRooms from "./axios/getRoom";
-
+import Arrow from '../assets/arrow.svg';
 const Home = () => {
   const navigate = useNavigate();
   // Mapping identifiers to actual image imports
@@ -19,11 +19,11 @@ const Home = () => {
     br1: br1,
     br2: br2,
   };
-
+  const [translate, setTranslate] = useState("translate-x-[50%]");
   const [rooms, setRooms] = useState([])
   const [image, setImage] = useState([])
   const location = useLocation();
-
+  const [isSlide, setSlide] = useState(false)
   useEffect(() => {
     getRooms()
       .then((res) => {
@@ -58,7 +58,7 @@ const Home = () => {
     return (
       <img
         src={`http://localhost:8000/storage/images/${file_name}`}
-        className="object-cover w-full h-3/4 rounded-xl"
+        className="object-cover w-full h-full px-1 py-1"
         alt={room.room_name}
         key={room_id}
         onClick={(e) => {
@@ -69,6 +69,17 @@ const Home = () => {
     );
   };
 
+  const slide = () => {
+    if (!isSlide) {
+      setTranslate("translate-x-[134%]")
+      setSlide(true)
+    }
+    if (isSlide) {
+      setTranslate("translate-x-[50%]")
+      setSlide(false)
+    }
+   
+  }
   useEffect(() => {
     if (location.hash) {
       const id = location.hash.replace("#", "");
@@ -78,6 +89,7 @@ const Home = () => {
       }
     }
   }, [location]);
+
   return (
     <>
       <Header />
@@ -95,7 +107,7 @@ const Home = () => {
             Available Rooms
           </h1>
         </div>
-        <div className="grid lg:grid-cols-3 grid-cols-2 mt-3 p-3 justify-center">
+        <div className="grid lg:grid-cols-3 grid-cols-2 mt-3 mb-5 p-3 justify-center " >
           {rooms.map((room) => (
             <div
               id="rooms"
@@ -104,19 +116,22 @@ const Home = () => {
             >
               <div
                 id="roomEdit"
-                className="lg:w-4/5 w-full lg:h-[100%]  h-full  relative bg-white rounded-xl Rounded-xl gap-4 
-                    border transfrom duration-75 ease-in-out transform  hover:scale-105 shadow-xl"
-              >
+                className={`lg:w-4/5 w-full lg:h-[100%]  h-full flex  bg-gray-100 gap-4 
+                   transfrom duration-75 ease-in-out transform shadow-sm z-10`}
+               >
                 {/* <Link to="/book"> */}
                 {renderImage(room, room.id)}
-                <h1 className="m-1 g-2 text-actText lg:text-xl md:text-xl text-sm font-semibold ">
-                  {room.room_name}
-                </h1>
-                <h2 className="m-1 g-2 text-darkText lg:text-xl md:text-xl text-sm">
-                  ₱ {room.price}
-                </h2>
+                <div className={`absolute  h-full w-3/4 px-1 py-2  hover:transition-all flex flex-col border-none -z-10 ${translate} hover:opacity-100`}
+                onClick={slide}>
+                  <label htmlFor="">Room #: {room.room_name} </label>
+                  <label htmlFor="">Price: {room.price} </label>
+                  <label htmlFor="">Max Guest: {room.maximum_guest} </label>
+                      
+               </div>
+                
                 {/* </Link> */}
               </div>
+              +
             </div>
           ))}
         </div>
